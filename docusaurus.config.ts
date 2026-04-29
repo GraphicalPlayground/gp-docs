a9d68fdf4a843be65b840ab5bb373491e6fa860d
mallory-scotton <mallory.scotton@epitech.eu>
2026-04-29 09:12:41 +0200

    chore: add tailwindcss support for docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,6 +38,9 @@ const config: Config = {
 
   trailingSlash: false,
 
+  // Plugins
+  plugins: ["./src/plugins/tailwind-config.js"],
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
